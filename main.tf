19de129e05efd2c2320d3165e9ad47e679c554ad
Kenny <85456335+kalabi1@users.noreply.github.com>
2022-02-20 18:21:12 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -155,9 +155,7 @@ resource "aws_instance" "Docker_Host" {
       "sudo curl -L \"https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)\" -o /usr/local/bin/docker-compose",
       "sudo chmod +x /usr/local/bin/docker-compose",
       "sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose",
-      "docker-compose --version",
       "sudo docker-compose up -d",
-      "sudo docker-compose scale wordpress=4 -d",
     ]
   }
 
